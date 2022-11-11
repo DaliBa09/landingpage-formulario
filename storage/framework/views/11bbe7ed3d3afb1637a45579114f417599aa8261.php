4d3331470e20b669ab5013b3a672fc8c6533d3a2
DaliBa09 <111331595+DaliBa09@users.noreply.github.com>
2022-11-11 13:29:12 -0600

    Add files via upload

---
+++ storage/framework/views/11bbe7ed3d3afb1637a45579114f417599aa8261.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Formulario</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+        <div class="container" style="background-color: #EEEEEE;">
+            <a class="navbar-brand text-uppercase text-dark fw-bold d-lg-none" href="/bienvenido">Menu</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" maria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item px-lg-4"><a class="nav-link text-dark text-uppercase" href="/bienvenido">Inicio</a></li>
+                    <li class="nav-item px-lg-4"><a class="nav-link text-dark text-uppercase" href="/contacto">Contacto</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="card md-3 mx-auto" style="max-width: 540px;">
+		<div class="row no-gutters align-middle">
+			<div class="col align-middle">
+				<div class="card-body">
+					<h5 class="card-title">Contacto</h5>
+					<form action="login.php" method="POST">
+						<div class="form-group">
+                            <?php if(!empty($lleno)): ?>
+                            <?php endif; ?>
+							<label for="nombre">Ingresa tu nombre</label>
+							<input type="text" required name="nombre" value="<?php echo e($persona); ?>" class="form-control">
+							<label for="correo">Ingersa tu correo</label>
+							<input type="email" required name="correo" value="<?php echo e($correo); ?>" class="form-control">
+                            <label for="codigo">Codigo de alumno</label>
+                            <input type="number" name="codigo" class="form-control">
+                            <label for="comentario">Agrega un comentario</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" required></textarea>
+						</div>
+						<br>
+                        <button class="btn btn-primary" type="submit" value="Ingresar">Registrar</button>
+					</form>		
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html><?php /**PATH C:\laragon\www\comedor_logan\resources\views/contacto.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/5adfc58e99178ac8b9f25b477590310f87a32c66.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Lorem ipsum, dolor sit amet consectetur 
+    adipisicing elit. Commodi adipisci eum, dicta 
+    mollitia rerum quo, dolorum molestias aut vel 
+    illum, cum laboriosam! Dolorum dolorem 
+    voluptatibus velit mollitia repellat porro nam!
+</body>
+</html><?php /**PATH C:\laragon\www\comedor_logan\resources\views/bienvenido.blade.php ENDPATH**/ ?>
